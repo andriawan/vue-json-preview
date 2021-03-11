3a776b5dac0eff9034e4908c2a31db828bc84ce1
Muhammad Irwan Andriawan <irwan@andriawan.com>
2021-03-11 16:39:27 +0700

    feat(vue-component): add sample componen in serve.vue

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,40 @@
 <script lang="ts">
-import Vue from 'vue';
-import VueJsonPreview from '@/vue-json-preview.vue';
+import Vue from "vue";
+import VueJsonPreview from "@/vue-json-preview.vue";
 
 export default Vue.extend({
-  name: 'ServeDev',
+  name: "ServeDev",
+  data() {
+    return {
+      position: "BOTTOM_LEFT",
+      data: {},
+    };
+  },
+  methods: {
+    setPosition(position: string) {
+      this.position = position;
+      this.data = position;
+    },
+    loadDataFromInternet(): void {
+      this.data = "Processing...";
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((json) => (this.data = json));
+    },
+  },
   components: {
-    VueJsonPreview
-  }
+    VueJsonPreview,
+  },
 });
 </script>
 
 <template>
   <div id="app">
-    <vue-json-preview />
+    <vue-json-preview :data="data" :position="position" />
+    <button @click="setPosition('BOTTOM_LEFT')">BOTTOM LEFT</button>
+    <button @click="setPosition('BOTTOM_RIGHT')">BOTTOM RIGHT</button>
+    <button @click="setPosition('TOP_LEFT')">TOP LEFT</button>
+    <button @click="setPosition('TOP_RIGHT')">TOP RIGHT</button>
+    <button @click="loadDataFromInternet">LOAD DATA FROM INTERNET</button>
   </div>
 </template>
