3d1b281adafa0e217822789b0f59e17dba5ef97e
Muhammad Irwan Andriawan <irwan@andriawan.com>
2021-03-11 15:14:08 +0700

    feat(vue-component): add vue component json preview

--- src/vue-json-preview.vue
+++ src/vue-json-preview.vue
@@ -1,88 +1,247 @@
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
-interface SampleData {
-  counter: number;
-  initCounter: number;
-  message: {
-    action: string | null;
-    amount: number | null;
-  };
+enum DISPLAY_TYPE {
+  MAXIMIZE = "MAXIMIZE",
+  MINIMIZE = "MINIMIZE",
 }
 
-export default /*#__PURE__*/Vue.extend({
-  name: 'VueJsonPreview', // vue component name
-  data(): SampleData {
+enum DISPLAY_THEME_TYPE {
+  DARK = "DARK",
+  LIGHT = "LIGHT",
+}
+
+enum POSITION {
+  TOP_LEFT = "TOP_LEFT",
+  TOP_RIGHT = "TOP_RIGHT",
+  BOTTOM_LEFT = "BOTTOM_LEFT",
+  BOTTOM_RIGHT = "BOTTOM_RIGHT",
+}
+
+interface Data {
+  display: String;
+  displayTheme: String;
+}
+
+export default /*#__PURE__*/ Vue.extend({
+  name: "VueJsonPreview", // vue component name
+  props: {
+    data: {
+      type: [Object, Array, String, Boolean],
+      default: [],
+    },
+    position: {
+      type: String,
+      default: POSITION.BOTTOM_RIGHT,
+    },
+  },
+  data(): Data {
     return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null,
-      },
+      display: DISPLAY_TYPE.MAXIMIZE,
+      displayTheme: DISPLAY_THEME_TYPE.DARK,
     };
   },
   computed: {
-    changedBy() {
-      const { message } = this as SampleData;
-      if (!message.action) return 'initialized';
-      return `${message.action} ${message.amount || ''}`.trim();
+    class_theme(): Object {
+      return {
+        "display-theme-dark": this.displayTheme === DISPLAY_THEME_TYPE.DARK,
+        "display-theme-light": this.displayTheme === DISPLAY_THEME_TYPE.LIGHT,
+      };
+    },
+    object_theme(): Object {
+      return {
+        light: this.displayTheme === DISPLAY_THEME_TYPE.LIGHT,
+        dark: this.displayTheme === DISPLAY_THEME_TYPE.DARK,
+      };
+    },
+    viewer_theme(): Object {
+      return {
+        "viewer-light": this.displayTheme === DISPLAY_THEME_TYPE.LIGHT,
+        "viewer-dark": this.displayTheme === DISPLAY_THEME_TYPE.DARK,
+      };
+    },
+    label_display(): String {
+      return this.display === DISPLAY_TYPE.MAXIMIZE ? "minimize" : "maximize";
+    },
+    label_display_theme(): String {
+      return this.displayTheme === DISPLAY_THEME_TYPE.DARK ? "light" : "dark";
+    },
+    is_display_maximize(): Boolean {
+      return this.display === DISPLAY_TYPE.MAXIMIZE;
+    },
+    position_style(): Object {
+      return {
+        "viewer-bottom-right": this.position === POSITION.BOTTOM_RIGHT,
+        "viewer-bottom-left": this.position === POSITION.BOTTOM_LEFT,
+        "viewer-top-right": this.position === POSITION.TOP_RIGHT,
+        "viewer-top-left": this.position === POSITION.TOP_LEFT,
+      };
     },
   },
   methods: {
-    increment(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
+    toggleDisplay() {
+      if (this.display === DISPLAY_TYPE.MAXIMIZE) {
+        this.display = DISPLAY_TYPE.MINIMIZE;
+        return;
+      }
+
+      if (this.display === DISPLAY_TYPE.MINIMIZE) {
+        this.display = DISPLAY_TYPE.MAXIMIZE;
+        return;
+      }
     },
-    decrement(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-    reset(): void {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
+    toggleTheme() {
+      if (this.displayTheme === DISPLAY_THEME_TYPE.DARK) {
+        this.displayTheme = DISPLAY_THEME_TYPE.LIGHT;
+        return;
+      }
+
+      if (this.displayTheme === DISPLAY_THEME_TYPE.LIGHT) {
+        this.displayTheme = DISPLAY_THEME_TYPE.DARK;
+        return;
+      }
     },
   },
 });
 </script>
 
 <template>
-  <div class="vue-json-preview">
-    <p>The counter was {{ changedBy }} to <b>{{ counter }}</b>.</p>
-    <button @click="increment">
-      Click +1
-    </button>
-    <button @click="decrement">
-      Click -1
-    </button>
-    <button @click="increment(5)">
-      Click +5
-    </button>
-    <button @click="decrement(5)">
-      Click -5
-    </button>
-    <button @click="reset">
-      Reset
-    </button>
+  <div ref="viewer" :class="[viewer_theme, position_style]">
+    <div class="display-status" @click="toggleDisplay">
+      <p>
+        {{ label_display }}
+      </p>
+    </div>
+    <div :class="class_theme" @click="toggleTheme">
+      <p>
+        {{ label_display_theme }}
+      </p>
+    </div>
+    <div v-if="is_display_maximize">
+      <template v-if="Array.isArray(data)">
+        <template v-for="(item, index) in data">
+          <div :class="object_theme" :key="index">
+            <div>{{ item.constructor.name }}</div>
+            {{ JSON.stringify(item, null, 4) }}
+            <br />
+          </div>
+        </template>
+      </template>
+      <template v-else>
+        <div :class="object_theme">
+          {{ JSON.stringify(data, null, 4) }}
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
 <style scoped>
-  .vue-json-preview {
-    display: block;
-    width: 400px;
-    margin: 25px auto;
-    border: 1px solid #ccc;
-    background: #eaeaea;
-    text-align: center;
-    padding: 25px;
-  }
-  .vue-json-preview p {
-    margin: 0 0 1em;
-  }
+.display-status {
+  display: inline-block;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  border-radius: 0.3rem;
+  background-color: #dd6b20;
+  position: sticky;
+  color: #e2e8f0;
+  top: 0;
+  right: 0;
+}
+
+.display-theme-dark {
+  display: inline-block;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  border-radius: 0.3rem;
+  background-color: #1d1d1d;
+  position: sticky;
+  color: #e2e8f0;
+  margin-left: 0.3rem;
+  top: 0;
+  right: 0;
+}
+
+.display-theme-light {
+  display: inline-block;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  border-radius: 0.3rem;
+  background-color: #ffffff;
+  position: sticky;
+  color: #2d3748;
+  margin-left: 0.3rem;
+  top: 0;
+  right: 0;
+}
+
+.display-theme-dark:hover,
+.display-theme-light:hover {
+  cursor: pointer;
+}
+
+.display-status:hover {
+  background-color: #fda467;
+  cursor: pointer;
+}
+
+.dark,
+.light {
+  border-radius: 0.5rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+}
+
+.dark {
+  background-color: #1d1d1d;
+  box-shadow: #1d1d1d 0rem 0.1rem 1rem;
+}
+
+.light {
+  background-color: #f6f6f6;
+  box-shadow: #cacaca 0rem 0.1rem 1rem;
+}
+
+.viewer-dark,
+.viewer-light {
+  padding: 0.9rem;
+  white-space: pre;
+  font-family: monospace;
+  border-radius: 0.5rem;
+  position: fixed;
+  margin: 1rem;
+  max-height: 20rem;
+  max-width: 40rem;
+  overflow: auto;
+}
+
+.viewer-bottom-right {
+  bottom: 0;
+  right: 0;
+}
+
+.viewer-bottom-left {
+  bottom: 0;
+  left: 0;
+}
+
+.viewer-top-right {
+  top: 0;
+  right: 0;
+}
+
+.viewer-top-left {
+  top: 0;
+  left: 0;
+}
+
+.viewer-dark {
+  color: #00ff62;
+  background-color: #2d3748;
+}
+.viewer-light {
+  color: #9e9e9e;
+  background-color: #eaeaea;
+}
 </style>
