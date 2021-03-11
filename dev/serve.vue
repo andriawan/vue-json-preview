646f7e83bb5536ccb11fcce3f44e993848282748
Muhammad Irwan Andriawan <irwan@andriawan.com>
2021-03-11 21:04:02 +0700

    feat(vue-main): improve layouting demo

--- dev/serve.vue
+++ dev/serve.vue
@@ -31,10 +31,40 @@ export default Vue.extend({
 <template>
   <div id="app">
     <vue-json-preview :data="data" :position="position" />
-    <button @click="setPosition('BOTTOM_LEFT')">BOTTOM LEFT</button>
-    <button @click="setPosition('BOTTOM_RIGHT')">BOTTOM RIGHT</button>
-    <button @click="setPosition('TOP_LEFT')">TOP LEFT</button>
-    <button @click="setPosition('TOP_RIGHT')">TOP RIGHT</button>
-    <button @click="loadDataFromInternet">LOAD DATA FROM INTERNET</button>
+    <button @click="setPosition('BOTTOM_LEFT')">
+      <span>BOTTOM LEFT</span>
+    </button>
+    <button @click="setPosition('BOTTOM_RIGHT')">
+      <span>BOTTOM RIGHT</span>
+    </button>
+    <button @click="setPosition('TOP_LEFT')"><span>TOP LEFT</span></button>
+    <button @click="setPosition('TOP_RIGHT')"><span>TOP RIGHT</span></button>
+    <button @click="loadDataFromInternet">
+      <span>LOAD DATA FROM INTERNET</span>
+    </button>
   </div>
 </template>
+<style scoped>
+#app {
+  width: 100%;
+}
+
+#app span {
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+#app button {
+  cursor: pointer;
+  margin-right: 10px;
+  padding: 8px;
+  color: white;
+  background-color: blue;
+  border: none;
+  border-radius: 5px;
+}
+
+#app button:hover {
+  background-color: rgb(4, 4, 150);
+}
+</style>
